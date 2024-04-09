d85b5fc1f76cac3c7bec9782b795d069e69c49b1
Andrey Czerwinski dos Santos <74416189+andrey-app@users.noreply.github.com>
2024-04-09 13:12:46 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+const botaoEsconderProjetos = document.querySelector('.btn-esconder-projetos');
+
+botaoEsconderProjetos.classList.add("remover");
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    esconderBotao();
+    botaoEsconderProjetos.classList.remove("remover");
+});
+
+botaoEsconderProjetos.addEventListener('click', () => {
+    esconderProjetos();
+    mostrarBotao();
+    botaoEsconderProjetos.classList.add("remover");
+});
+
+function mostrarBotao() {
+    botaoMostrarProjetos.classList.remove("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
+
+function esconderProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.remove('ativo');
+    });
+}
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add('remover');
+}
+
+
+
+
